021ba2f9af6f471530db604b6ef7c0bafd4667e5
mine260309 <mine260309@9e8d2898-ca56-a880-7485-cac695b98a92>
2010-09-05 07:34:54 +0000

    Use shared preference to store settings. Replace the pictures with meaningful ones, but still ugly...

--- trunk/src/com/mine/MineMessageVibrator.java
+++ trunk/src/com/mine/MineMessageVibrator.java
@@ -1,48 +1,28 @@
 package com.mine;
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Vibrator;
 
 
 public class MineMessageVibrator {
-	private static final int MINE_MESSAGE_NOTIFICATION_ID = 0x1086;
-	private static Notification smsNotify = null;
-	private static Notification mmsNotify = null;
+//	private static final int MINE_MESSAGE_NOTIFICATION_ID = 0x1086;
+//	private static Notification smsNotify = null;
+//	private static Notification mmsNotify = null;
 	
 	public static void notifySMS(Context context) {
 		MineLog.v("notifying SMS");
-
-//		createNotification();
-//		NotificationManager nm =
-//	        (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-//		nm.notify(MINE_MESSAGE_NOTIFICATION_ID, smsNotify);
 		vibrate(context);
+		
+		//TODO: maybe I can use notification instead
 	}
 	
 	public static void notifyMMS(Context context) {
 		MineLog.v("notifying MMS");
-		
-//		createNotification();
-//		NotificationManager nm =
-//	        (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-//		nm.notify(MINE_MESSAGE_NOTIFICATION_ID, smsNotify);
 		vibrate(context);
+
+		//TODO: maybe I can use notification instead
 	}
-	
-	private static void createNotification() {
-		if (smsNotify == null) {
-			smsNotify = new Notification();
-			//smsNotify.defaults = Notification.DEFAULT_VIBRATE;
-			smsNotify.vibrate = new long[] {0, 1000};
-		}
-		if (mmsNotify == null) {
-			mmsNotify = smsNotify;
-		}
-	}
+
 	private static void vibrate(Context context) {
 		Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 		v.vibrate(1000);
--- trunk/src/com/mine/MineVibrationSetting.java
+++ trunk/src/com/mine/MineVibrationSetting.java
@@ -1,28 +1,20 @@
 package com.mine;
 
 import android.app.Activity;
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.pm.*;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.*;
 import android.widget.AdapterView.OnItemClickListener;
 import android.view.*;
-import android.media.AudioManager;
 
 public class MineVibrationSetting extends Activity {
 	
 	private ListView mainSettingListView;
 	private ArrayAdapter<String> mainListAdapter;
-	private AudioManager mineAudioManager;
 	private Context mineContext;
-	private static final int VibrationModes[] = {AudioManager.VIBRATE_SETTING_OFF,
-			AudioManager.VIBRATE_SETTING_ON,
-			AudioManager.VIBRATE_SETTING_ONLY_SILENT};
-	private static final int RingerModes[] = {AudioManager.RINGER_MODE_NORMAL,
-		AudioManager.RINGER_MODE_SILENT,
-		AudioManager.RINGER_MODE_VIBRATE
-	};
+	
+	private String ToogleVibrationSetting = "Toogle Vibration";
 	
 	private boolean messageCheckerEnabled = false;
 	
@@ -32,129 +24,35 @@ public class MineVibrationSetting extends Activity {
         super.onCreate(savedInstanceState);
         
         mineContext = this;
-        mineAudioManager = (AudioManager) mineContext.getSystemService(Context.AUDIO_SERVICE);
         mainSettingListView = new ListView(this);
         
         mainListAdapter = new ArrayAdapter<String>(this, R.layout.textview);
         mainSettingListView.setAdapter(mainListAdapter);
-    	mainListAdapter.add("Toogle Setting");
-//    	mainListAdapter.add("Current mode: " 
-//    			+ getVibrationString(mineAudioManager.getVibrateSetting(AudioManager.VIBRATE_TYPE_NOTIFICATION)));
-//    			+ getRingerString(mineAudioManager.getRingerMode()));
+    	mainListAdapter.add(ToogleVibrationSetting);
     	
     	mainSettingListView.setOnItemClickListener(new OnItemClickListener() {
 			    public void onItemClick(AdapterView<?> parent, View view,
 			        int position, long id) {
-			      // When clicked, do something
-			    	String menuItem = ((TextView) view).getText().toString();
-			    	if (menuItem == "Toogle Setting") {
-			    		//ToogleVibrationMode();
-			    		//ToogleRingerMode();
+			        String menuItem = ((TextView) view).getText().toString();
+			    	if (menuItem == ToogleVibrationSetting) {
 			    		ToogleMessageChecker();
 			    	}
 			    }
 			  });
-		setContentView(mainSettingListView);
-		
-        //setContentView(R.layout.main);
+    	initVibrationmode();
+		setContentView(mainSettingListView);		
     }
     
-    private String getVibrationString(int type) {
-    	String virbTypeStr;
-		switch(type) {
-		case AudioManager.VIBRATE_SETTING_OFF:
-			virbTypeStr = "Vibration OFF";
-			break;
-		case AudioManager.VIBRATE_SETTING_ON:
-			virbTypeStr = "Vibration ON";
-			break;
-		case AudioManager.VIBRATE_SETTING_ONLY_SILENT:
-			virbTypeStr = "Vibration ONLY SILENT";
-			break;
-		default:
-			virbTypeStr = "What the hell?";
-		    break;			    	
-		}
-		return virbTypeStr;
-    }
-    private String getRingerString(int type) {
-    	String ringerTypeStr;
-    	switch(type) {
-    	case AudioManager.RINGER_MODE_NORMAL:
-    		ringerTypeStr = "Ringer NORMAL";
-    		break;
-    	case AudioManager.RINGER_MODE_SILENT:
-    		ringerTypeStr = "Ringer SILENT";
-    		break;
-    	case AudioManager.RINGER_MODE_VIBRATE:
-    		ringerTypeStr = "Ringer VIBRATE";
-    		break;
-    	default:
-    		ringerTypeStr = "What the hell?";
-    		break;
+    private void initVibrationmode() {
+    	messageCheckerEnabled = MineVibrationToggler.GetVibrationMode(mineContext);
+    	if (messageCheckerEnabled) {
+    		EnableMessageChecker();
     	}
-    	return ringerTypeStr;
-    }
-    
-    private void ToogleVibrationMode() {
-    	int virbType = mineAudioManager.getVibrateSetting(AudioManager.VIBRATE_TYPE_NOTIFICATION);
-		int virbMode;
-		String virbTypeStr;
-		switch (virbType) {
-		case AudioManager.VIBRATE_SETTING_OFF:
-			virbTypeStr = "Vibration OFF";
-			virbMode = 0;
-			break;
-		case AudioManager.VIBRATE_SETTING_ON:
-			virbTypeStr = "Vibration ON";
-			virbMode = 1;
-			break;
-		case AudioManager.VIBRATE_SETTING_ONLY_SILENT:
-			virbTypeStr = "Vibration ONLY SILENT";
-			virbMode = 2;
-			break;
-		default:
-			virbTypeStr = "What the hell?";
-		    virbMode = 0;
-		    break;
-		}
-		while (mainListAdapter.getCount() > 1)
-		    mainListAdapter.remove(mainListAdapter.getItem(mainListAdapter.getCount()-1));
-		mainListAdapter.add("Previous mode: " + virbTypeStr);
-		mineAudioManager.setVibrateSetting(AudioManager.VIBRATE_TYPE_NOTIFICATION, 
-				VibrationModes[(++virbMode)%3]);
-		virbTypeStr = getVibrationString(VibrationModes[virbMode%3]);
-		mainListAdapter.add("Set to: " + virbTypeStr);
-    }
-    
-    private void ToogleRingerMode() {
-    	int ringerType = mineAudioManager.getRingerMode();
-		int ringerMode;
-		String ringerTypeStr;
-		switch (ringerType) {
-		case AudioManager.RINGER_MODE_NORMAL:
-			ringerTypeStr = "Ringer NORMAL";
-			ringerMode = 0;
-			break;
-		case AudioManager.RINGER_MODE_SILENT:
-			ringerTypeStr = "Ringer SILENT";
-			ringerMode = 1;
-			break;
-		case AudioManager.RINGER_MODE_VIBRATE:
-			ringerTypeStr = "Ringer VIBRATE";
-			ringerMode = 2;
-			break;
-		default:
-			ringerTypeStr = "What the hell?";
-			ringerMode = 0;
-		    break;
-		}
-		while (mainListAdapter.getCount() > 1)
-		    mainListAdapter.remove(mainListAdapter.getItem(mainListAdapter.getCount()-1));
-		mainListAdapter.add("Previous mode: " + ringerTypeStr);
-		mineAudioManager.setRingerMode( RingerModes[(++ringerMode)%3]);
-		ringerTypeStr = getRingerString(RingerModes[ringerMode%3]);
-		mainListAdapter.add("Set to: " + ringerTypeStr);
+    	else {
+    		DisableMessageChecker();
+    	}
+		mainListAdapter.add("Message Vibration: " + 
+				(messageCheckerEnabled?"ON":"OFF"));
     }
     
     private void EnableMessageChecker() {
@@ -168,17 +66,22 @@ public class MineVibrationSetting extends Activity {
     
     private void ToogleMessageChecker() {
     	
-		while (mainListAdapter.getCount() > 1)
+    	if (messageCheckerEnabled) {
+    		messageCheckerEnabled = false;
+    		DisableMessageChecker();
+    	} else {
+    		messageCheckerEnabled = true;
+    		EnableMessageChecker();
+    	}
+    	
+    	// Set the mode in the screen
+		while (mainListAdapter.getCount() > 1) {
 		    mainListAdapter.remove(mainListAdapter.getItem(mainListAdapter.getCount()-1));
+		}
 		mainListAdapter.add("Message Vibration: " + 
 				(messageCheckerEnabled?"ON":"OFF"));
 		
-    	if (messageCheckerEnabled) {
-    		EnableMessageChecker();
-    		messageCheckerEnabled = false;
-    	} else {
-    		DisableMessageChecker();
-    		messageCheckerEnabled = true;
-    	}
+		Intent intent = new Intent(MineVibrationToggler.VIBRATION_ACTION_NAME);
+		mineContext.sendBroadcast(intent);
     }
 }
