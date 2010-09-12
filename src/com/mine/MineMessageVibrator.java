9d04bf58e22590a5fdf673cefd8f886283d88795
mine260309 <mine260309@9e8d2898-ca56-a880-7485-cac695b98a92>
2010-09-12 15:43:28 +0000

    Vibration mode support: short, middle, long, multiple short, multiple long TODO: Use different vibration mode for incoming message & reminder.

--- src/com/mine/MineMessageVibrator.java
+++ src/com/mine/MineMessageVibrator.java
@@ -1,7 +1,9 @@
 package com.mine;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Vibrator;
+import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
 
 
@@ -9,26 +11,35 @@ public class MineMessageVibrator {
 //	private static final int MINE_MESSAGE_NOTIFICATION_ID = 0x1086;
 //	private static Notification smsNotify = null;
 //	private static Notification mmsNotify = null;
+	private static final int VIBRATE_REASON_SMS = 0;
+	private static final int VIBRATE_REASON_MMS = VIBRATE_REASON_SMS;
+	private static final int VIBRATE_REASON_REMINDER = 2;
+	
+	private static final long[] VibratePatternShort = new long[] {0, 500};
+	private static final long[] VibratePatternMiddle = new long[] {0, 1200};
+	private static final long[] VibratePatternLong = new long[] {0, 2000};
+	private static final long[] VibratePatternMultipleShort = new long[] {0,500,200,500,200,500};
+	private static final long[] VibratePatternMultipleMiddle = new long[] {0,1200,300,1200,300,1200};
 	
 	public static void notifySMS(Context context) {
 		MineLog.v("notifying SMS");
-		vibrate(context);
+		vibrate(context, VIBRATE_REASON_SMS);
 
 		//TODO: maybe I can use notification instead
 	}
 	
 	public static void notifyMMS(Context context) {
 		MineLog.v("notifying MMS");
-		vibrate(context);
+		vibrate(context, VIBRATE_REASON_MMS);
 
 		//TODO: maybe I can use notification instead
 	}
 
 	public static void notifyReminder(Context context) {
 		MineLog.v("notifying reminder");
-		vibrate(context);
+		vibrate(context, VIBRATE_REASON_REMINDER);
 	}
-	
+/*
 	private static void vibrate(Context context) {
 	    // Get phone state, if not idle then don't vibrate
 	    TelephonyManager mTM = 
@@ -37,5 +48,47 @@ public class MineMessageVibrator {
 	    	Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 	    	v.vibrate(1000);
 	    }
+	}
+*/
+	private static void vibrate(Context context, int reason) {
+	    // Get phone state, if not idle then don't vibrate
+	    TelephonyManager mTM = 
+	      (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+	    if (mTM.getCallState()== TelephonyManager.CALL_STATE_IDLE) {
+	    	Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+	    	v.vibrate(GetVibratePattern(context, reason), -1);
+	    }
+	}
+	
+	private static long[] GetVibratePattern(Context context, int reason){
+		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+		String pattern = settings.getString(context.getString(R.string.pref_vibration_mode_key), "Middle");
+		long[] pat;
+		if(pattern.equals("Short")) {
+			pat = VibratePatternShort;
+			MineLog.v("Vibrate using pattern "+pattern);
+		}
+		else if (pattern.equals("Middle")) {
+			pat = VibratePatternMiddle;
+			MineLog.v("Vibrate using pattern "+pattern);
+		}
+		else if (pattern.equals("Long")) {
+			pat = VibratePatternLong;
+			MineLog.v("Vibrate using pattern "+pattern);
+		}
+		else if (pattern.equals("Multiple Short")) {
+			pat = VibratePatternMultipleShort;
+			MineLog.v("Vibrate using pattern "+pattern);
+		}
+		else if (pattern.equals("Multiple Middle")) {
+			pat = VibratePatternMultipleMiddle;
+			MineLog.v("Vibrate using pattern "+pattern);
+		}
+		else {
+			//default, use middle
+			pat = VibratePatternMiddle;
+			MineLog.v("Vibrate using pattern "+"default");
+		}
+		return pat;
 	}
 }
