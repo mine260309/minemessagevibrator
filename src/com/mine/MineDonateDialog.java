9c2c4d45e33cdcdc8691d8514f365c77bb9a4898
mine260309 <mine260309@9e8d2898-ca56-a880-7485-cac695b98a92>
2010-10-20 12:14:32 +0000

    Add Donate button in preference list.

---
+++ src/com/mine/MineDonateDialog.java
@@ -0,0 +1,25 @@
+package com.mine;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+public class MineDonateDialog extends DialogPreference {
+
+	private Context context;
+	public static final Uri DONATE_URI = Uri.parse("http://mine-message-vibrator.appspot.com");
+	public MineDonateDialog(Context c, AttributeSet attrs) {
+		super(c, attrs);
+		context = c;
+	}
+
+	@Override
+	protected void onDialogClosed (boolean positiveResult) {
+		if (positiveResult) {
+			Intent i = new Intent(Intent.ACTION_VIEW, DONATE_URI);
+			context.startActivity(i);
+		}
+	}
+}
