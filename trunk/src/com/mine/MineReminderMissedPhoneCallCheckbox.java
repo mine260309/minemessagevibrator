eed45569709a19244e0ae9f09dffadefa04d0fae
mine260309 <mine260309@9e8d2898-ca56-a880-7485-cac695b98a92>
2010-12-23 04:26:24 +0000

    Add missed .java file...

---
+++ trunk/src/com/mine/MineReminderMissedPhoneCallCheckbox.java
@@ -0,0 +1,34 @@
+package com.mine;
+
+import android.content.Context;
+import android.preference.CheckBoxPreference;
+import android.util.AttributeSet;
+
+public class MineReminderMissedPhoneCallCheckbox extends CheckBoxPreference {
+	private Context context;
+
+	public MineReminderMissedPhoneCallCheckbox(Context c) {
+		super(c);
+		context = c;
+	}
+
+	public MineReminderMissedPhoneCallCheckbox(Context c,
+			AttributeSet attrs) {
+		super(c, attrs);
+		context = c;
+	}
+
+	public MineReminderMissedPhoneCallCheckbox(Context c,
+			AttributeSet attrs, int defStyle) {
+		super(c, attrs, defStyle);
+		context = c;
+	}
+
+	@Override
+	protected void onClick() {
+		super.onClick();
+		//MineVibrationToggler.SetMissedPhoneCallReminderEnabled(context, isChecked());
+		//MineVibrationSetting.AdjustPreference();
+	}
+
+}
