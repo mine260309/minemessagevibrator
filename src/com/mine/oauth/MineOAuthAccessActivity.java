a040fb231c4c944f73809fc6332a8d885196641c
mine260309@gmail.com <mine260309@gmail.com@9e8d2898-ca56-a880-7485-cac695b98a92>
2011-12-17 05:34:45 +0000

    1) Implement gmail authentication better. Now it is OK if activity is killed during authentication. 2) Re-enable the code of start/stop gmail watcher
    TODO:
    1) Need to verify the user name whether match the system's gmail account or not;
    2) Show a loading dialog during authenticate's token retrieve. If network is bad/unreachable, it should show the loading picture, and tell user it's unable to authenticate.

--- src/com/mine/oauth/MineOAuthAccessActivity.java
+++ src/com/mine/oauth/MineOAuthAccessActivity.java
@@ -1,5 +1,23 @@
-package com.mine.oauth;
+/*************************************************************************
+ * MineMessageVibrator is an Android App that provides vibrate and 
+ * reminder functions for SMS, MMS, Gmail, etc. 
+ * Copyright (C) 2010  Lei YU
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ************************************************************************/
 
+package com.mine.oauth;
 
 import com.mine.MineLog;
 import com.mine.MineRingerModeChangeReceiver;
@@ -18,7 +36,7 @@ import android.widget.Button;
 public class MineOAuthAccessActivity extends Activity {
 	private final static String LOGTAG = "MineOAuth";
 	
-	private OAuthHelper mHelper;
+	private static OAuthHelper mHelper;
 	public static String[] mAccessToken = null;
 	
 	@Override
@@ -50,21 +68,30 @@ public class MineOAuthAccessActivity extends Activity {
 		boolean ret =false;
 		super.onNewIntent(intent);
 	    Uri uri = intent.getData();
-
+	    // TODO: check if gmail username matches system's gmail user 
 	    if (uri != null) {
 		    //String token = uri.getQueryParameter("oauth_token");
 		    String verifier = uri.getQueryParameter("oauth_verifier");
-		    
+		    Log.v(LOGTAG, "get verifier: " + verifier);
 			try {
+				if (mHelper==null) {
+					mHelper = OAuthHelper.load(getApplicationContext());
+					if (mHelper == null) {
+						Log.e(LOGTAG, "mHelper failed to load!");
+						throw new Exception("mHelper failed to load!");
+					}
+				}
 				mAccessToken = mHelper.getAccessToken(verifier);
 				Log.v(LOGTAG, "get token: " + mAccessToken[0] +", " + mAccessToken[1]);
 				MineVibrationToggler.SaveGmailToken(this, mAccessToken);
+				MineVibrationToggler.SetUnreadGmailReminderEnabled(this,true);
 				ret = true;
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
 	    }
 		finish();
+		
 		if (ret) {
 			Intent intent1 = new Intent(MineRingerModeChangeReceiver.ACTION_GMAIL_TOKEN_CALLBACK);
 			MineLog.v("Send gmail token callback intent");
@@ -77,15 +104,17 @@ public class MineOAuthAccessActivity extends Activity {
         	Thread t = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                try {
-        			mHelper = new OAuthHelper("anonymous", "anonymous", "", "");
-
-            	    String uri = mHelper.getRequestToken();
-            	    startActivity(new Intent("android.intent.action.VIEW", Uri.parse(uri)).
-            	    		setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_FROM_BACKGROUND));
-            	} catch (Exception ex) {
-            		ex.printStackTrace();
-            	}
+	                try {
+	    				mHelper = new OAuthHelper("anonymous", "anonymous", "", "");
+	            	    //TODO: show a loading dialog
+	            	    String uri = mHelper.getRequestToken();
+	            	    OAuthHelper.save(getApplicationContext(),mHelper);
+	
+	            	    startActivity(new Intent("android.intent.action.VIEW", Uri.parse(uri)).
+	            	    		setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_FROM_BACKGROUND));
+	            	} catch (Exception ex) {
+	            		ex.printStackTrace();
+	            	}
                 }
         	});
         	t.start();
@@ -94,3 +123,4 @@ public class MineOAuthAccessActivity extends Activity {
 		}
 	}
 }
+
